1dec5d15b9243079f940e938bbcfc1b2a4d2a15b
Albert Chubakov <supervtb@gmail.com>
2018-03-22 17:34:36 +0300

    Initial Commit

---
+++ commicsv1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  commicsv1
+//
+//  Created by Альберт Чубаков on 22.03.2018.
+//  Copyright © 2018 Альберт Чубаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
