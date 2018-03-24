2d8be2bc92fb0623d0d852cddee6499d5d4dc4a7
Albert Chubakov <supervtb@gmail.com>
2018-03-25 00:44:37 +0300

    0.01

---
+++ commicsv1/Models/TypeOfLayouts.swift
@@ -0,0 +1,16 @@
+//
+//  TypeOfLayouts.swift
+//  commicsv1
+//
+//  Created by Альберт Чубаков on 24.03.2018.
+//  Copyright © 2018 Альберт Чубаков. All rights reserved.
+//
+
+import Foundation
+class TypeOfLayouts {
+    var nameLayout = ""
+    
+    init(nameLayout: String) {
+        self.nameLayout = nameLayout
+    }
+}
